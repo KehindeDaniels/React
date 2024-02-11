127184ad8ffbb996c8ad4c4a1530baa8897ccb2a
kehindeDaniels <kehindedaniels501@gmail.com>
2024-02-11 13:13:28 +0100

    Added a clearAll button

--- src/components/AddItems.jsx
+++ src/components/AddItems.jsx
@@ -3,7 +3,7 @@ import React, {useState} from 'react'
 
 
 const AddItems = () => {
- const [items, setitems] = useState(['Thing 1', 'Thing 2'])
+ const [items, setitems] = useState([])
  
  const ItemElement = items.map((item, index)=> (
     <p
@@ -14,19 +14,24 @@ const AddItems = () => {
  ))
 
  function handleAddItem(){
-    setitems(prevItem=>[...prevItem, `Thing ${prevItem.length + 1}`])
+    setitems(prevItem=>[...prevItem, `Item ${prevItem.length + 1}`])
 }
 
  function handleRemoveItem(){
     setitems(prevItem=>prevItem.slice(0, -1))
  }
+
+ function handleClearItem(){
+    setitems(prevItem=>prevItem.slice(-1, -1))
+ }
   return (
-    <>
+    <> 
         <div className="flex flex-col bg-slate-400 p-4 rounded-md font-bold">
             {ItemElement}
         </div>
         <button onClick={handleAddItem} className='mt-8 bg-slate-400 py-2 px-8 rounded-full'>Add items</button>
         <button onClick={handleRemoveItem} className='mt-8 ml-8 bg-slate-400 py-2 px-8 rounded-full'>Remove items</button>
+        <button onClick={handleClearItem} className='mt-8 ml-8 bg-slate-400 py-2 px-8 rounded-full'>Clear all</button>
     </>
   )
 }
